a8a68e6787e0b3c7e69964c42c61238c998cf14a
Justin Giancola <justin.giancola@gmail.com>
2023-10-28 13:54:49 +0000

    Move work live view to a component and display in the main view

---
+++ lib/underwork_web/live/work_component.ex
@@ -0,0 +1,37 @@
+defmodule UnderworkWeb.WorkComponent do
+  use UnderworkWeb, :live_component
+
+  alias Underwork.Cycles
+
+  def update(%{cycle: cycle}, socket) do
+    changeset = Cycles.change_cycle_plan(cycle)
+    form = to_form(changeset)
+
+    socket =
+      socket
+      |> assign(:cycle, cycle)
+      |> assign(:form, form)
+
+    {:ok, socket}
+  end
+
+  def handle_event("review", _, %{assigns: %{cycle: cycle}} = socket) do
+    case Cycles.finish_work(cycle) do
+      {:ok, cycle} ->
+        socket =
+          socket
+          |> put_flash(:info, "YAY")
+
+        send(self(), {__MODULE__, {:cycle_changed, cycle}})
+
+        {:noreply, socket}
+
+      {:error, changeset} ->
+        socket =
+          socket
+          |> assign(:form, to_form(changeset))
+
+        {:noreply, socket}
+    end
+  end
+end
