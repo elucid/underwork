1d6369e8e3f679c2bdfab4ff42bdde6bc187c617
Justin Giancola <justin.giancola@gmail.com>
2023-11-05 11:33:46 +0000

    No longer used

--- lib/underwork_web/helpers.ex
+++ lib/underwork_web/helpers.ex
@@ -1,18 +1,2 @@
 defmodule UnderworkWeb.Helpers do
-  use Phoenix.VerifiedRoutes,
-    endpoint: UnderworkWeb.Endpoint,
-    router: UnderworkWeb.Router
-
-  def next_cycle_path(session, cycle) do
-    cond do
-      cycle == nil ->
-        ~p"/sessions/#{session.id}/review"
-      cycle.state == "planning" ->
-        ~p"/sessions/#{session.id}/cycle/#{cycle.id}/plan"
-      cycle.state == "working" ->
-        ~p"/sessions/#{session.id}/cycle/#{cycle.id}/work"
-      cycle.state == "reviewing" ->
-        ~p"/sessions/#{session.id}/cycle/#{cycle.id}/review"
-    end
-  end
 end
