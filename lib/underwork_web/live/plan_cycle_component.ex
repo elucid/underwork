9cb799ac9e671fe3571d5296bdbecb89950a8d86
Justin Giancola <justin.giancola@gmail.com>
2023-12-11 22:12:41 +0000

    Extract morale button component

--- lib/underwork_web/live/plan_cycle_component.ex
+++ lib/underwork_web/live/plan_cycle_component.ex
@@ -33,6 +33,15 @@ defmodule UnderworkWeb.PlanCycleComponent do
     {:noreply, assign(socket, form: to_form(changeset))}
   end
 
+  def handle_event("set_morale", %{"morale" => morale}, socket) do
+    changeset =
+      socket.assigns.cycle
+      |> Cycles.change_cycle_assessment(%{morale: morale})
+      |> Map.put(:action, :validate)
+
+    {:noreply, assign(socket, form: to_form(changeset))}
+  end
+
   def handle_event("save", %{"cycle" => params}, socket) do
     cycle = socket.assigns.cycle
 
@@ -64,6 +73,15 @@ defmodule UnderworkWeb.PlanCycleComponent do
     <.button type="button"
       phx-click="set_energy" phx-value-energy={value} phx-target={phx_target}
       class={if Phoenix.HTML.Form.input_value(form, :energy) == value, do: "bg-zinc-500", else: "bg-zinc-200"}
+      phx-prevent-default><%= label %></.button>
+    """
+  end
+
+  def morale_button(%{value: value, label: label, form: form, "phx-target": phx_target} = assigns) do
+    ~H"""
+    <.button type="button"
+      phx-click="set_morale" phx-value-morale={value} phx-target={phx_target}
+      class={if Phoenix.HTML.Form.input_value(form, :morale) == value, do: "bg-zinc-500", else: "bg-zinc-200"}
       phx-prevent-default><%= label %></.button>
     """
   end
